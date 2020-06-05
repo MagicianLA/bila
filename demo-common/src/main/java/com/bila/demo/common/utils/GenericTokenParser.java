1eba955230bb6bc2d3af659b3173ba69f2af0af6
Bila <zhanzhengqiang@alibaba-inc.com>
2020-06-05 19:24:49 +0800

    demo

--- demo-common/src/main/java/com/bila/demo/common/utils/GenericTokenParser.java
+++ demo-common/src/main/java/com/bila/demo/common/utils/GenericTokenParser.java
@@ -1,5 +1,11 @@
 package com.bila.demo.common.utils;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+
+import org.springframework.util.StringUtils;
+
 /**
  * @Author: Magician
  * @Desc:
@@ -16,90 +22,71 @@ public class GenericTokenParser {
      * @param args
      * @return
      */
-    public static String parse(String openToken, String closeToken, String text, Object... args) {
-        if (args == null || args.length <= 0) {
+    public static String parse(String openToken, String closeToken, String text, Map<String, Object> args) {
+        if (StringUtils.isEmpty(text)) {
             return text;
         }
-        int argsIndex = 0;
-        if (text == null || text.isEmpty()) {
-            return "";
+        if (Objects.isNull(args) || args.size() == 0) {
+            return text;
         }
-        char[] src = text.toCharArray();
+
         int offset = 0;
-        // search open token
         int start = text.indexOf(openToken, offset);
         if (start == -1) {
             return text;
         }
+
         final StringBuilder builder = new StringBuilder();
         StringBuilder expression = null;
+
+        char[] src = text.toCharArray();
+        int openTokenLength = openToken.length();
         while (start > -1) {
             if (start > 0 && src[start - 1] == '\\') {
-                // this open token is escaped. remove the backslash and continue.
                 builder.append(src, offset, start - offset - 1).append(openToken);
-                offset = start + openToken.length();
+                offset = start + openTokenLength;
             } else {
-                // found open token. let's search close token.
-                if (expression == null) {
+                if (Objects.isNull(expression)) {
                     expression = new StringBuilder();
                 } else {
                     expression.setLength(0);
                 }
                 builder.append(src, offset, start - offset);
-                offset = start + openToken.length();
+                offset = start + openTokenLength;
                 int end = text.indexOf(closeToken, offset);
                 while (end > -1) {
                     if (end > offset && src[end - 1] == '\\') {
-                        // this close token is escaped. remove the backslash and continue.
                         expression.append(src, offset, end - offset - 1).append(closeToken);
                         offset = end + closeToken.length();
                         end = text.indexOf(closeToken, offset);
                     } else {
                         expression.append(src, offset, end - offset);
-                        offset = end + closeToken.length();
                         break;
                     }
                 }
                 if (end == -1) {
-                    // close token was not found.
                     builder.append(src, start, src.length - start);
                     offset = src.length;
                 } else {
-                    ///////////////////////////////////////仅仅修改了该else分支下的个别行代码////////////////////////
-                    String value = (argsIndex <= args.length - 1) ?
-                        (args[argsIndex] == null ? "" : args[argsIndex].toString()) : expression.toString();
-                    builder.append(value);
+                    String varKey = text.substring(start + openTokenLength, end);
+                    builder.append(args.get(varKey));
                     offset = end + closeToken.length();
-                    argsIndex++;
-                    ////////////////////////////////////////////////////////////////////////////////////////////////
                 }
             }
             start = text.indexOf(openToken, offset);
         }
+
         if (offset < src.length) {
             builder.append(src, offset, src.length - offset);
         }
         return builder.toString();
     }
-    public static String parse0(String text, Object... args) {
-        return GenericTokenParser.parse("${", "}", text, args);
-    }
-    public static String parse1(String text, Object... args) {
-        return GenericTokenParser.parse("{", "}", text, args);
-    }
-    /**
-     * 使用示例
-     * @param args
-     */
-    public static void main(String... args) {
-        //{}被转义，不会被替换
-        System.out.println(GenericTokenParser.parse("{", "}", "我的名字是\\{},结果是{}，可信度是%{}", "雷锋", true, 100));
-        System.out.println(GenericTokenParser.parse0("我的名字是${},结果是${}，可信度是%${}", "雷锋", true, 100));
-        System.out.println(GenericTokenParser.parse1("我的名字是{},结果是{}，可信度是%{}", "雷锋", true, 100));
-        //    输出结果如下：
-        //    我的名字是{},结果是true，可信度是%100
-        //    我的名字是雷锋,结果是true，可信度是%100
-        //    我的名字是雷锋,结果是true，可信度是%100
+
+    public static void main(String[] args) {
+        Map<String, Object> paramMap = new HashMap<>();
+        paramMap.put("NAME", "张三");
+        paramMap.put("RESULT", "GOOD");
+        System.out.println(parse("{", "}", "TEST\\{}我的名字是${NAME},结果是${RESULT}，可信度是%{}", paramMap));
     }
 }
 
